9b9b132d022c901534979f3057c223df65bf920c
milindoka <oak445@gmail.com>
2025-11-20 08:12:49 +0530

    Made Burambad BDR as main page

--- html/Marklist-Mob/app.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('#menu-toggle');
-const menuList = document.querySelector('#menu-list');
-
-menuToggle.addEventListener('click', function() {
-    menuList.classList.toggle('active');
-});
